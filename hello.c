02cb8357282b4b1108e0af330aff9df0434d1884
aschramunl <aschram4@huskers.unl.edu>
2026-08-27 22:52:54 +0000

    inital commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Aiden Schram
+ *         aschram4@huskers.unl.edu
+ * Date: 2026/08/27
+ *
+ * A  program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Aiden Schram\n");
+
+  return 0;
+}
